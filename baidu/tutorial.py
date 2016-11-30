d44e822df61a3561293696d8199d3bb10300c6c4
jessicawjr <jinrong.wu@symbio.com>
2016-11-30 14:38:32 +0800

    adjust the style of the code

--- baidu/tutorial.py
+++ baidu/tutorial.py
@@ -2,8 +2,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-
-
 TIMEOUT = 30
 
 
